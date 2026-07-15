34a3bebeabedcbef1a228d065c68bfe9153b69be
ateeq786316 <ateeq786316@gmail.com>
2026-07-15 04:31:08 -0700

    Initialize backend with Express, add essential middleware, and set up server

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Backend running'));
+
+module.exports = app;
